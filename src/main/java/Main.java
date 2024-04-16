5f973ac40c641428e8bcbfc3301098fb0375761e
Szeng89 <shijinz898@gmail.com>
2024-04-15 21:25:17 -0400

    split via ## for now and wrote it to a new output.txt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import org.apache.commons.io.IOUtils;
 import java.io.IOException;
+import java.io.FileWriter;
 import java.util.Arrays;
 
 public class Main {
@@ -15,11 +16,30 @@ public class Main {
         String output = (new Main()).readRawDataToString();
 
         String[] items = output.split("##");
-        for (String item : items) {
-            System.out.println(item);
+//        for (String item : items) {
+
+        try (FileWriter writer = new FileWriter("output2.txt")) {
+            writer.write("[\n"); // Start of JSON array
+
+            for (int i = 0; i < items.length; i++) {
+                String value = items[i];
+
+                // Add quotes around strings
+                if (value instanceof String) {
+                    value = "\"" + value + "\"";
+                }
+
+                writer.write("  " + value);
+
+                //nextline
+                if (i != items.length - 1) {
+                    writer.write("\n");
+                }
+            }
+
+            writer.write("\n]"); // End of JSON array
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
-
-
-
 }
